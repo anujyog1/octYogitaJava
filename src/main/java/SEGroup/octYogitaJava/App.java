a5b5ae21fdad6365dcbee744afed9d901ba2f204
yogita <agyogita@gmail.com>
2023-10-31 12:26:40 +0530

    first commit

---
+++ src/main/java/SEGroup/octYogitaJava/App.java
@@ -0,0 +1,13 @@
+package SEGroup.octYogitaJava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello all today is 31 Oct 2023" );
+    }
+}
